85ecff3d013a264239cb8c5f3023c859e9469728
julian.peternell <peternell.julian@gmail.com>
2025-06-28 16:24:04 +0200

    added docs folder

--- Marianne-Peternell/vite.config.ts
+++ Marianne-Peternell/vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(({ command }) => {
   const config = {
     plugins: [react(), tailwindcss()],
     base: "/",
+    build: {
+      outDir: "docs",
+    },
   };
 
   if (command !== "serve") {
