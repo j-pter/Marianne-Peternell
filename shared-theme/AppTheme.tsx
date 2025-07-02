8dc10d65e5ad7e12c82bff94b96462606fcaccd6
julian.peternell <peternell.julian@gmail.com>
2025-07-03 01:13:27 +0200

    + disable dark mode for now + rebuilt site

--- shared-theme/AppTheme.tsx
+++ shared-theme/AppTheme.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import type { ThemeOptions } from "@mui/material/styles";
-// import { inputsCustomizations } from "./customizations/inputs";
-// import { feedbackCustomizations } from "./customizations/feedback.tsx";
-// import { navigationCustomizations } from "./customizations/navigation.tsx";
 import { surfacesCustomizations } from "./customizations/surfaces.ts";
 import { colorSchemes, typography, shadows, shape } from "./themePrimitives.ts";
 
@@ -25,7 +22,7 @@ export default function AppTheme(props: AppThemeProps) {
       : createTheme({
           // For more details about CSS variables configuration, see https://mui.com/material-ui/customization/css-theme-variables/configuration/
           cssVariables: {
-            colorSchemeSelector: "data-mui-color-scheme",
+            // colorSchemeSelector: "data-mui-color-scheme",
             cssVarPrefix: "template",
           },
           colorSchemes, // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
